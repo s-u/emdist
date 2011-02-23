e2856cdaa9594a899609ad3d5f34cffa2195e302
Simon Urbanek <simon.urbanek@r-project.org>
2011-02-23 22:01:29 +0000

    add emd3d

--- R/emd.R
+++ R/emd.R
@@ -1,1 +1,5 @@
 emd <- function(A, B, xdist = 1, ydist = 1) .Call("emd_2d", A, B, xdist, ydist, PACKAGE="emd")
+
+emd3d <- function(A, B) .Call("emd_3d", A, B, PACKAGE="emd")
+
+emd3dw <- function(A, wA, B, wB, ...) emd3d(cbind(wA, A), cbind(wB, B), ...)
