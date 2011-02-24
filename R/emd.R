dfd59561e77855d41ad8cebda50b416e78d4073c
Simon Urbanek <simon.urbanek@r-project.org>
2011-02-24 16:18:18 +0000

    fix emdw and add an example

--- R/emd.R
+++ R/emd.R
@@ -2,4 +2,4 @@ emd2d <- function(A, B, xdist = 1, ydist = 1) .Call("emd_2d", A, B, xdist, ydist
 
 emd <- function(A, B, ...) .Call("emd_4d", A, B, PACKAGE="emd")
 
-emdw <- function(A, wA, B, wB, ...) emd3d(cbind(wA, A), cbind(wB, B), ...)
+emdw <- function(A, wA, B, wB, ...) emd(cbind(wA, A), cbind(wB, B), ...)
