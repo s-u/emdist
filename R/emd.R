c6116721866f1e087a2d0c2514824cfd14020aa8
Simon Urbanek <simon.urbanek@r-project.org>
2011-02-24 16:05:50 +0000

    fix 3d code, up it to 4d and rename APIs

--- R/emd.R
+++ R/emd.R
@@ -1,5 +1,5 @@
-emd <- function(A, B, xdist = 1, ydist = 1) .Call("emd_2d", A, B, xdist, ydist, PACKAGE="emd")
+emd2d <- function(A, B, xdist = 1, ydist = 1) .Call("emd_2d", A, B, xdist, ydist, PACKAGE="emd")
 
-emd3d <- function(A, B) .Call("emd_3d", A, B, PACKAGE="emd")
+emd <- function(A, B, ...) .Call("emd_4d", A, B, PACKAGE="emd")
 
-emd3dw <- function(A, wA, B, wB, ...) emd3d(cbind(wA, A), cbind(wB, B), ...)
+emdw <- function(A, wA, B, wB, ...) emd3d(cbind(wA, A), cbind(wB, B), ...)
